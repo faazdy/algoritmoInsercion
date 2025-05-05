b8e7ad562f5a0554a355dd35900346e4a83cda45
faazdy <leorengifor@gmail.com>
2025-05-04 19:40:16 -0500

    -m 'mejor json'

--- app.ts
+++ app.ts
@@ -6,12 +6,18 @@ const BASE_URL = 'https://servapibi.xm.com.co';
 async function fetchAndSave(endpoint: string, filename: string, payload: object) {
   try {
     const { data } = await axios.post(`${BASE_URL}/${endpoint}`, payload);
-    await writeJson(`./data/${filename}.json`, data, { spaces: 2 });
+    console.log(`🔍 Respuesta de ${endpoint}:`, data);
+
+    //asegurar el array de oiobjetos en json
+    const normalizado = Array.isArray(data) ? data : data?.datos || data?.items || [data];
+
+    await writeJson(`./data/${filename}.json`, normalizado, { spaces: 2 });
     console.log(`✅ Datos de "${endpoint}" guardados en ${filename}.json`);
   } catch (error: any) {
     console.error(`❌ Error en ${endpoint}:`, error.message);
   }
 }
+
 
 async function run() {
   await fetchAndSave('hourly', 'hourly', {
@@ -27,13 +33,15 @@ async function run() {
     StartDate: '2025-02-01',
     EndDate: '2025-02-28',
     Entity: 'Recurso',
-    Filter: []
+    Filter: [
+      "Codigo Submercado Generación"
+    ]
   });
 
   await fetchAndSave('monthly', 'monthly', {
     MetricId: 'FAER',
     StartDate: '2023-01-01',
-    EndDate: '2024-01-21',
+    EndDate: '2024-02-28',
     Entity: 'Sistema',
     Filter: [] 
   });
